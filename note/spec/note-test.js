efa91b4e4c8229a2fa1105602a8653579ed9ca0d
ejatkin <ejatkin@gmail.com>
2017-01-06 14:02:26 +0000

    Started challenge 9, first test passing

--- note/spec/note-test.js
+++ note/spec/note-test.js
@@ -3,7 +3,14 @@
 function testNewNoteCreated() {
   var note = new Note('hi!');
   console.log("TEST: testNewNoteCreated")
-  assert.isTrue( note.readNote() === 'hi!')
+  assert.isTrue(note.readNote() === 'hi!')
+};
+
+function testNewNoteIdCreated() {
+  var note = new Note("text of note", 0);
+  console.log("TEST: testNewNoteIdCreated")
+  assert.isTrue(note.returnId() === 0)
 };
 
 testNewNoteCreated();
+testNewNoteIdCreated();
--- note/src/note-model.js
+++ note/src/note-model.js
@@ -2,12 +2,17 @@
 
 (function(exports) {
 
-  function Note(text) {
+  function Note(text, id) {
     this._text = text;
+    this._id = id;
   };
 
   Note.prototype.readNote = function() {
     return this._text;
+  };
+
+  Note.prototype.returnId = function() {
+    return this._id;
   };
 
   exports.Note = Note;
